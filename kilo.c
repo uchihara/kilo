bf06b7be0518f01186a90251c70598315a2dfeb6
Akira Uchihara <akira.uchihara@every.tv>
2024-10-25 16:50:24 +0900

    Add exit to ctrl-q

--- kilo.c
+++ kilo.c
@@ -6,6 +6,9 @@
 #include <termios.h>
 #include <unistd.h>
 
+/** defines **/
+#define CTRL_KEY(k) ((k) & 0x1f)
+
 /** data **/
 struct termios orig_termios;
 
@@ -50,7 +53,7 @@ int main(void) {
     } else {
       printf("%d ('%c')\r\n", c, c);
     }
-    if (c == 'q') break;
+    if (c == CTRL_KEY('q')) break;
   }
   return 0;
 }
