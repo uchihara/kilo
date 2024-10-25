1f8905882cc01b6eb0de57a36cb4c767f5732524
Akira Uchihara <akira.uchihara@every.tv>
2024-10-25 16:58:47 +0900

    Add tildes

--- kilo.c
+++ kilo.c
@@ -53,8 +53,19 @@ char editorReadKey(void) {
 
 /** output **/
 
+void editorDrawRows(void) {
+  int y;
+  for (y = 0; y < 24; y++) {
+    write(STDOUT_FILENO, "~\r\n", 3);
+  }
+}
+
 void editorRefreshScreen(void) {
   write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+
+  editorDrawRows();
+
   write(STDOUT_FILENO, "\x1b[H", 3);
 }
 
