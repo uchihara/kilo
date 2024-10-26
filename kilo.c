b61c17c133331de81eb1d89f9c843610ac86695a
Akira Uchihara <akira.uchihara@every.tv>
2024-10-26 09:40:07 +0900

    Add snap to end of lie

--- kilo.c
+++ kilo.c
@@ -293,6 +293,8 @@ void editorRefreshScreen(void) {
 /** input **/
 
 void editorMoveCursor(int key) {
+  erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+
   switch (key) {
     case ARROW_LEFT:
       if (E.cx != 0) {
@@ -300,7 +302,9 @@ void editorMoveCursor(int key) {
       }
       break;
     case ARROW_RIGHT:
-      E.cx++;
+      if (row && E.cx < row->size) {
+        E.cx++;
+      }
       break;
     case ARROW_UP:
       if (E.cy != 0) {
@@ -312,6 +316,12 @@ void editorMoveCursor(int key) {
         E.cy++;
       }
       break;
+  }
+
+  row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+  int rowlen = row ? row->size : 0;
+  if (E.cx > rowlen) {
+    E.cx = rowlen;
   }
 }
 
